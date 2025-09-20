eff318f99635201af8ec41df40f60043a8a01be8
Jayson Knight <jayson.knight@jaysonknight.com>
2025-09-20 01:37:17 -0400

    Minor refactoring

---
+++ Kiota/TopstepClientFactory.cs
@@ -0,0 +1,34 @@
+﻿using Kiota;
+
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+namespace Kiota;
+
+/// <summary>
+/// 
+/// </summary>
+public class GitHubClientFactory
+{
+    private readonly IAuthenticationProvider _authenticationProvider;
+    private readonly HttpClient _httpClient;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="httpClient"></param>
+    public GitHubClientFactory(HttpClient httpClient)
+    {
+        _authenticationProvider = new AnonymousAuthenticationProvider();
+        _httpClient = httpClient;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public ApiClient GetClient()
+    {
+        return new ApiClient(new HttpClientRequestAdapter(_authenticationProvider, httpClient: _httpClient));
+    }
+}
